2aad3b96384102fc7c6e1f6409dad4a0d5769b61
IlyaT21 <ilijatoskovic@gmail.com>
2025-01-30 14:09:08 +0100

    Completed for now, in plans to add images and fix special characters

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ import PDFDocument from "pdfkit";
 
 // Launch the browser and open a new blank page
 const browser = await puppeteer.launch({
-  headless: false,
   defaultViewport: false,
 });
 const page = await browser.newPage();
@@ -48,7 +47,7 @@ for (let i = 0; i < elements.length; i++) {
 }
 
 // Loop through the Emperors
-for (let i = 0; i < 3; i++) {
+for (let i = 0; i < emperors.length; i++) {
   // Click on the element
   await emperors[i].click();
 
@@ -70,26 +69,31 @@ for (let i = 0; i < 3; i++) {
     const thExists = await row.$("th");
     if (thExists) {
       thText = await row.$eval("th", (th) => th.innerText.trim());
+
+      pdfDoc.fontSize(10).text(`${thText}`, { underline: true });
     }
 
     // Check for <td> and extract its text
     const tdExists = await row.$("td");
     if (tdExists) {
       tdText = await row.$eval("td", (td) => td.innerText.trim());
+
+      // Check for a subheading
+      const subheadingClass = await row.$eval("td", (td) =>
+        td.classList.contains("infobox-subheader")
+      );
+
+      if (subheadingClass) {
+        pdfDoc.text(`${tdText}`);
+      } else {
+        pdfDoc.moveDown(0.5).text(`${tdText}`);
+      }
     }
 
-    // Log the results
-    console.log(`Row ${i + 1}:`);
-    console.log(`  TH: ${thText}`);
-    console.log(`  TD: ${tdText}`);
-
-    // Add the row's data to the PDF
-    pdfDoc
-      .fontSize(10)
-      .text(`${thText}`, { underline: true })
-      .moveDown(0.5)
-      .text(`${tdText}`)
-      .moveDown(1);
+    // Add space for next row, unless it's empty
+    if (tdExists || thExists) {
+      pdfDoc.moveDown(1);
+    }
   }
 
   // Go back to the previous page to process the next emperor
@@ -99,10 +103,13 @@ for (let i = 0; i < 3; i++) {
   emperors = await page.$$(
     'th[style="text-align:center; background:#F8F9FA"] a[title]'
   );
+
+  // Show number of Emperors fetched in the terminal
+  console.log(`${i + 1} Emperors fetched`);
 }
 
 // Finalize the PDF
 pdfDoc.end();
-console.log(`PDF created successfully: ${pdfFileName}`);
+console.log(`Process completed!`);
 
-// await browser.close();
+await browser.close();
